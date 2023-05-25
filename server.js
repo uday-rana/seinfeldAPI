5a1dc37a54acf6f7c8607e55e1b5de24cab7c6c6
Glitch (jade-simple-nose) <none>
2023-05-25 01:27:05 +0000

    Add start script for Glitch hosting

--- server.js
+++ server.js
@@ -9,21 +9,24 @@ app.use(cors());
 app.use(express.static(`public`));
 
 app.get(`/`, (req, res) => {
-	res.json({ message: "Welcome to the Seinfeld API. For a random quote, go to /random. For all quotes, go to /all." });
+  res.json({
+    message:
+      "Welcome to the Seinfeld API. For a random quote, go to /random. For all quotes, go to /all.",
+  });
 });
 
 app.get(`/random`, (req, res) => {
-	res.json(quotes[Math.floor(Math.random() * quotes.length)]);
+  res.json(quotes[Math.floor(Math.random() * quotes.length)]);
 });
 
 app.get(`/all`, (req, res) => {
-	res.json(quotes);
+  res.json(quotes);
 });
 
 app.use((req, res) => {
-	res.status(404).json({ error: "Seinfeld API returned status code 404" });
+  res.status(404).json({ error: "Seinfeld API returned status code 404" });
 });
 
 app.listen(HTTP_PORT, () => {
-	console.log(`Seinfeld API listening on port ${HTTP_PORT}`);
+  console.log(`Seinfeld API listening on port ${HTTP_PORT}`);
 });
